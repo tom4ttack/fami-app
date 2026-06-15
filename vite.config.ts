88a473c3ea589ea5b2cee91a16b24b5133fa4d83
tom4ttack <kids406070@gmail.com>
2026-06-15 10:42:37 +0900

    0615 firebase 수정

--- vite.config.ts
+++ vite.config.ts
@@ -66,7 +66,7 @@ self.addEventListener("notificationclick", function(event) {
 function figmaAssetResolver() {
   return {
     name: 'figma-asset-resolver',
-    resolveId(id) {
+    resolveId(id: string) {
       if (id.startsWith('figma:asset/')) {
         const filename = id.replace('figma:asset/', '')
         return path.resolve(__dirname, 'src/assets', filename)
